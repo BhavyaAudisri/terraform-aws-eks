55b0443c7877d2bbbf281ec31769537598dfbdcb
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-03-30 18:20:23 +0530

    code pushed to git

--- 20-bastion/userdata.sh
+++ 20-bastion/userdata.sh
@@ -34,3 +34,6 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+# K9S install
+curl -sS https://webinstall.dev/k9s | bash
