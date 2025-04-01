095afe49c62de241e6e6f3a5a8ebc9764e5d9bcb
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-02 00:40:32 +0530

    frontend updated

--- 60-alb/parameters.tf
+++ 60-alb/parameters.tf
@@ -3,8 +3,3 @@ resource "aws_ssm_parameter" "alb_ingress_listener_arn" {
   type  = "String"
   value = aws_lb_listener.https.arn
 }
-resource "aws_ssm_parameter" "web_alb_certificate_arn" {
-  name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
-  type  = "String"
-  value = aws_acm_certificate.expense.arn
-}
