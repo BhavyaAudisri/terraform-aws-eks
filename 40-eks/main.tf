72172dfe286140e6146f250310ee35173073999c
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-01 18:06:26 +0530

    frontend updated

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -55,6 +55,7 @@ module "eks" {
         AmazonEC2FullAccess = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
         ElasticLoadBalancingReadOnly = "arn:aws:iam::aws:policy/ElasticLoadBalancingReadOnly"
         AWSShieldDRTAccessPolicy = "arn:aws:iam::aws:policy/service-role/AWSShieldDRTAccessPolicy"
+        EC2DescribeSecurityGroups = "arn:aws:iam::124355635734:policy/EC2DescribeSecurityGroups"
       }
     }
   }
