f873860defbb92a4e762374089307c41d16ba65b
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-02 00:30:43 +0530

    frontend updated

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -21,7 +21,7 @@ resource "aws_lb_listener" "https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local.alb_ingress_certificate_arn
+  certificate_arn   = aws_lb_listener.https.arn
 
   default_action {
     type = "fixed-response"
