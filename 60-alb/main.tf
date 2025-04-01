1d07460ed7a8239eebd8051c8f88e9d63f8caff4
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-02 01:11:37 +0530

    frontend updated

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -66,18 +66,18 @@ resource "aws_lb_target_group" "frontend" {
   }
 }
 
-# resource "aws_lb_listener_rule" "frontend" {
-#   listener_arn = data.aws_ssm_parameter.alb_ingress_certificate_arn.value
-#   priority     = 10
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = data.aws_ssm_parameter.alb_ingress_certificate_arn.value
+  priority     = 10
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.frontend.arn
-#   }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
 
-#   condition {
-#     host_header {
-#       values = ["expense-${var.environment}.${var.domain_name}"]
-#     }
-#   }
-# }
+  condition {
+    host_header {
+      values = ["expense-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
