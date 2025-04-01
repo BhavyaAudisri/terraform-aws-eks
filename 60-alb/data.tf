c1a318584e45c1da3a2edc938d748057225f0604
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-02 00:01:53 +0530

    frontend updated

--- 60-alb/data.tf
+++ 60-alb/data.tf
@@ -7,6 +7,6 @@ data "aws_ssm_parameter" "alb_ingress_sg_id" {
 data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public-subnet_ids"
 }
-# data "aws_ssm_parameter" "alb_ingress_certificate_arn" {
-#   name = "/${var.project_name}/${var.environment}/alb_ingress_certificate_arn"
-# }
+data "aws_ssm_parameter" "alb_ingress_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/alb_ingress_certificate_arn"
+}
