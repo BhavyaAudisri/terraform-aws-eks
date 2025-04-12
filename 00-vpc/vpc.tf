fb9eb723c9cab17e324bd3752aac2b5131ac238a
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-12 12:44:21 +0530

    eks upgrade

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -12,7 +12,7 @@ module "vpc" {
   #is_peering_required   = true
 }
 
-resource "aws_db_subnet_group" "expense" {
+/* resource "aws_db_subnet_group" "expense" {
   name = "${var.project_name}-${var.environment}"
   subnet_ids = module.vpc.database_subnet_ids
   tags = merge (
@@ -22,4 +22,4 @@ resource "aws_db_subnet_group" "expense" {
     }
   )
 }
-
+ */
